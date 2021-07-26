2c62146e3833b0f1ea90fa535e4a5ffb8431ed10
Blizzardbay <duttes@yahoo.com>
2021-07-26 16:52:56 -0400

    File Fix
    Fixed file problem.

---
+++ Intermediate/Build/Win64/SMProjectEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.29.30038
+#include "E:/Program Files (x86)/Unreal Projects/SMProject/Intermediate/Build/Win64/SMProjectEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
---
+++ Intermediate/Build/Win64/UE4Editor/Inc/SMProject/SMProjectGameModeBase.generated.h
@@ -0,0 +1,95 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef SMPROJECT_SMProjectGameModeBase_generated_h
+#error "SMProjectGameModeBase.generated.h already included, missing '#pragma once' in SMProjectGameModeBase.h"
+#endif
+#define SMPROJECT_SMProjectGameModeBase_generated_h
+
+#define SMProject_Source_SMProject_SMProjectGameModeBase_h_15_SPARSE_DATA
+#define SMProject_Source_SMProject_SMProjectGameModeBase_h_15_RPC_WRAPPERS
+#define SMProject_Source_SMProject_SMProjectGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define SMProject_Source_SMProject_SMProjectGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesASMProjectGameModeBase(); \
+	friend struct Z_Construct_UClass_ASMProjectGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(ASMProjectGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/SMProject"), NO_API) \
+	DECLARE_SERIALIZER(ASMProjectGameModeBase)
+
+
+#define SMProject_Source_SMProject_SMProjectGameModeBase_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesASMProjectGameModeBase(); \
+	friend struct Z_Construct_UClass_ASMProjectGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(ASMProjectGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/SMProject"), NO_API) \
+	DECLARE_SERIALIZER(ASMProjectGameModeBase)
+
+
+#define SMProject_Source_SMProject_SMProjectGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ASMProjectGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ASMProjectGameModeBase) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ASMProjectGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ASMProjectGameModeBase); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ASMProjectGameModeBase(ASMProjectGameModeBase&&); \
+	NO_API ASMProjectGameModeBase(const ASMProjectGameModeBase&); \
+public:
+
+
+#define SMProject_Source_SMProject_SMProjectGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ASMProjectGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ASMProjectGameModeBase(ASMProjectGameModeBase&&); \
+	NO_API ASMProjectGameModeBase(const ASMProjectGameModeBase&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ASMProjectGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ASMProjectGameModeBase); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ASMProjectGameModeBase)
+
+
+#define SMProject_Source_SMProject_SMProjectGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET
+#define SMProject_Source_SMProject_SMProjectGameModeBase_h_12_PROLOG
+#define SMProject_Source_SMProject_SMProjectGameModeBase_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	SMProject_Source_SMProject_SMProjectGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	SMProject_Source_SMProject_SMProjectGameModeBase_h_15_SPARSE_DATA \
+	SMProject_Source_SMProject_SMProjectGameModeBase_h_15_RPC_WRAPPERS \
+	SMProject_Source_SMProject_SMProjectGameModeBase_h_15_INCLASS \
+	SMProject_Source_SMProject_SMProjectGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define SMProject_Source_SMProject_SMProjectGameModeBase_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	SMProject_Source_SMProject_SMProjectGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	SMProject_Source_SMProject_SMProjectGameModeBase_h_15_SPARSE_DATA \
+	SMProject_Source_SMProject_SMProjectGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	SMProject_Source_SMProject_SMProjectGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+	SMProject_Source_SMProject_SMProjectGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> SMPROJECT_API UClass* StaticClass<class ASMProjectGameModeBase>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID SMProject_Source_SMProject_SMProjectGameModeBase_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
